8e10536c87c138f38b9ed2b8cae4838939cc3e9b
mohamadreza <mohamadrezamohammady2019@gmail.com>
2023-06-10 12:21:44 -0700

    clima connected to weather api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,11 +8,41 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+  }
 
-void getLocation() async{
-  Position? position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-  print(position);
-}
+  void getData() async {
+
+    var url = Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=a4d3feec9e573c25a0e853774ba5bc75');
+
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    }
+    else {
+      print(response.statusCode);
+    }
+  }
+
+//  this is Didmount method in flutter.
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+    getData();
+  }
+
+// this is Willunmount life cycle method used in flutter.
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +50,6 @@ void getLocation() async{
       body: Center(
         child: TextButton(
           onPressed: () {
-            getLocation();
             //Get the current location
           },
           child: Text('Get Location'),
